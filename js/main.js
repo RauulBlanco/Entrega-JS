a7ee6a2077a098a2ad955946537944dec220294e
Raul Blanco <blancorauul@gmail.com>
2023-08-28 20:02:31 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+!alert("Si gastas mas de $15000 10% descuento \nSi gastas mas de $20000 15% descuento\nSi gastas mas de $25000 20% descuento")
+
+function calcularDescuento() {
+  var totalGastos = 0;
+
+  while (true) {
+    var montoProducto = prompt("Ingrese el monto del producto (o ingrese 'fin' para finalizar):");
+
+    if (montoProducto.toLowerCase() === "fin") {
+      break;
+    }
+
+    montoProducto = parseFloat(montoProducto);
+
+    if (isNaN(montoProducto)) {
+      alert("Valor inválido. Intente nuevamente.");
+      continue;
+    }
+
+    totalGastos += montoProducto;
+  }
+
+  var descuento = 0;
+
+  if (totalGastos >= 15000) {
+    if (totalGastos >= 25000) {
+      descuento = 0.2; // 20% de descuento
+    } else if (totalGastos >= 20000) {
+      descuento = 0.15; // 15% de descuento
+    } else {
+      descuento = 0.1; // 10% de descuento
+    }
+  }
+
+  var porcentajeDescuento = descuento * 100;
+  var montoAPagar = totalGastos - (totalGastos * descuento);
+
+  alert("Monto a pagar después del descuento: $" + montoAPagar.toFixed(2) + "\nPorcentaje de descuento: " + porcentajeDescuento + "%");
+}
+
+// Llamar a la función para calcular el descuento
+calcularDescuento();
+
+
